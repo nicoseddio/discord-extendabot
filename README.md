a87d170a74ca955a5e7faa846b97517c6e2b3bec
Nico Seddio <nico.seddio@live.com>
2021-05-27 16:08:03 -0400

    basic app loading functionality

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # discord-extendabot
 A full redesign of what was the meow-bot for "The Jasmine Dragon" Discord server, the Extendabot is a simple Discord bot providing an interface for custom "apps": this bot can do anything!
+
+## setup
+meow-bot requires the following dependencies:  
+- `discord.js`
+
+Clone the repo and install dependencies with the following commands:  
+```
+npm install discord.js ffmpeg fluent-ffmpeg @discordjs/opus ytdl-core --save
+```
+
+## starting an instance
+First, ensure an `auth.json` file with your bot's token is present in the cloned base directory. A template file is available in `{base_dir}/lib/default/`.
+Then, from within the cloned directory, run:
+```
+node index.js
+```
+
+## resources
+[Discord Developer Portal](https://discord.com/developers/applications)
---
+++ index.js
@@ -0,0 +1,97 @@
+// Imports
+const Discord = require('discord.js');
+const fs = require('fs');
+
+// Inits
+const version = "0.0.000.000"; //Major, Minor, Build, Revision
+const client = new Discord.Client();
+const auth = require('./auth.json');
+const config = require('./config.json')
+const cache = {};
+
+// Startup
+client.login(auth.token);
+client.on('ready', () => {
+    console.log(`\nLogged in as ${client.user.tag}!`);
+    cache.apps = loadApps(config.apps,buildKernel());
+    console.log(`Loaded ${Object.keys(cache.apps).length} apps.`)
+    selftest();
+});
+
+// Functions
+function selftest() {
+    dumpSession(config,cache);
+    return;
+}
+function loadApps(appsConfig,kernel,dir='./lib/apps/') {
+    let cfg = appsConfig;
+    const aCache = {};
+    // load in directory
+    fs.readdirSync(dir)
+      .filter(a => a.endsWith('.js'))
+      .filter(a => !a.startsWith('._'))
+      .forEach(app => {
+            const appObj = require(dir+app);
+            cfg[app] = cfg[app] || {};
+            cfg[app].settings = cfg[app].settings || {};
+            aCache[app] = new appObj(kernel,cfg[app].settings);
+      }
+    );
+    return aCache;
+}
+
+function buildKernel() {
+    class kernel {
+    }
+    return new kernel();
+}
+
+function dumpSession(config,cache) {
+    objectToJSON(config,'dump_configBackup')
+    objectToJSON(cache, 'dump_cacheBackup')
+}
+
+function objectToJSON(object = this.cache,filename = 'dump_file') {
+    let data = JSON.stringify(replaceCircular(object), null, '    ');
+    fs.writeFile(filename+'.json', data, function (err) {
+        if (err) {
+            console.log(`filesave error: ${err.message}`);
+            return;
+        }
+        return true;
+    });
+}
+function createTimeStamp(date = new Date(),fileSystemSafe=false) {
+    let ts = `[` +
+        `${String(date.getFullYear()).substr(-2)}`      +
+        `${String(date.getMonth()+1).padStart(2,'0')}`  +
+        `${String(date.getDate()).padStart(2,'0')} `    +
+        `${String(date.getHours()).padStart(2,'0')}:`   +
+        `${String(date.getMinutes()).padStart(2,'0')}:` +
+        `${String(date.getSeconds()).padStart(2,'0')}`  +
+    `]`;
+    if (fileSystemSafe) {
+        ts =  ts.replaceAll(':','-')
+                .replaceAll(' ','_')
+                .replaceAll('[','')
+                .replaceAll(']','')
+    }
+    return ts;
+}
+
+// from https://gist.github.com/saitonakamura/d51aa672c929e35cc81fa5a0e31f12a9
+// example: JSON.stringify(replaceCircular(obj))
+var replaceCircular = function(val, cache) {
+    cache = cache || new WeakSet();
+    if (val && typeof(val) == 'object') {
+        if (cache.has(val)) return '[Circular]';
+        cache.add(val);
+        var obj = (Array.isArray(val) ? [] : {});
+        for(var idx in val) {
+            obj[idx] = replaceCircular(val[idx], cache);
+        }
+        cache.delete(val);
+        return obj;
+    }
+    return val;
+};
